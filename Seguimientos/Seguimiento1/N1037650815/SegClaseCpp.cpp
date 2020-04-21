43032e8f784fa3192cf00139d3b1155a4dec22f5
andreavalenciac <andrea.valenciac@udea.edu.co>
2020-04-21 15:57:04 -0400

    Seguimiento1

--- Seguimientos/Seguimiento1/N1037650815/SegClaseCpp.cpp
+++ Seguimientos/Seguimiento1/N1037650815/SegClaseCpp.cpp
@@ -8,14 +8,13 @@ public:
   float X,Y,Z;
   float Vx,Vy,Vz;
   float M;
-  float Bz;
-  void SetValues(float,float,float,float,float,float,float,float,int);
+  void SetValues(float,float,float,float,float,float,float,int);
   float Pos_evol(float,float,float,float);
   
   
 }Par1,Par2;
 
-void Particula::SetValues(float x,float y,float z,float vx,float vy,float vz,float m, float bz, int C)
+void Particula::SetValues(float x,float y,float z,float vx,float vy,float vz,float m, int C)
 {
   X=x;
   Y=y;
@@ -24,16 +23,14 @@ void Particula::SetValues(float x,float y,float z,float vx,float vy,float vz,flo
   Vy=vy;
   Vz=vz;
   M=m;
-  Bz=bz;
   Carga=C;
 }
 
 float Particula::Pos_evol(float Fx,float Fy, float Fz,float t){
-  float w=Bz*Carga/M;
  
-  X=X+(Vx*t)+(0.5*Fx*(t*t));
-  Y=Y+(Vy*t)+(0.5*Fx*(t*t));
-  Z=Z+(Vz*t)+(0.5*Fx*(t*t));
+  X=X+(Vx*t)+(0.5*Fx*(t*t)/M);
+  Y=Y+(Vy*t)+(0.5*Fy*(t*t)/M);
+  Z=Z+(Vz*t)+(0.5*Fz*(t*t)/M);
   Vx=Vx+(Fx*t/M);
   Vy=Vy+(Fy*t/M);
   Vz=Vz+(Fz*t/M);
@@ -42,22 +39,28 @@ return 0;
 }
 int main(){
 
-  
-  Par1.SetValues(0.0,0.0,0.0,0.0,0.0,0.0,10.0,10.0,1);
-  Par2.SetValues(1.0,0.0,0.0,0.0,0.0,1.0,10.0,10.0,1);
-  for (int i=0; i<10000; i++){
+  float Bz=10.0;
+  float k0=8.99e9;
+  Par1.SetValues(0.0,0.0,0.0,0.0,0.0,0.0,10.0,1);
+  Par2.SetValues(1.0,0.0,0.0,0.0,0.0,0.0,10.0,-1);
+  for (int i=0; i<10; i++){
 
     
       
-    float Fx=Par1.Vy+1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
-    float Fy=-Par1.Vx+1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
-    float Fz=1/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
-    
-    Par1.Pos_evol(Fx,Fy,Fz,i/0.01);
-    Par2.Pos_evol(Fx,Fy,Fz,i/0.01);
+    float Fx1=Par1.Vy*Bz-k0*(Par1.X-Par2.X)/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),3/2);
+    float Fy1=-Par1.Vx*Bz-k0*(Par1.Y-Par2.Y)/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),3/2);
+    float Fz1=-k0*(Par1.Z-Par2.Z)/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
 
     
-    std::cout <<    Par1.X << "," << Par1.Y  << "," << Par1.Z  << "," <<    Par2.X << "," << Par2.Y  << "," << Par2.Z  << std::endl;
+    float Fx2=Par2.Vy*Bz+k0*(Par2.X-Par1.X)/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),3/2);
+    float Fy2=-Par2.Vx*Bz+k0*(Par2.Y-Par1.Y)/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),3/2);
+    float Fz2=k0*(Par2.Z-Par1.Z)/pow((pow((Par1.X-Par2.X),2)+pow((Par1.Y-Par2.Y),2)+pow((Par1.Z-Par2.Z),2)),1/2);
+    
+    Par1.Pos_evol(Fx1,Fy1,Fz1,0.01);
+    Par2.Pos_evol(-Fx2,-Fy2,-Fz2,0.01);
+
+    
+    std::cout << Par1.X << " " << Par1.Y  << " " << Par1.Z  << " " <<    Par2.X << " " << Par2.Y  << " " << Par2.Z  << std::endl;
   }
   return 0; }	
 
